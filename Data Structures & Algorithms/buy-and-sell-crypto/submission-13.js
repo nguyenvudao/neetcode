7731b20b4ae81c9f268718a43a3e8d851f5ce9e7
Vu Dao <22849827+nguyenvudao@users.noreply.github.com>
2026-06-12 12:31:50 +0100

    Add: buy-and-sell-crypto - submission-13

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-13.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        /**
+         * Brute force
+         */
+        // let maxProfit = 0
+        // for (let i = 0; i < prices.length; i++) {
+        //     for (let j = i + 1; j < prices.length; j++) {
+        //         maxProfit = Math.max(maxProfit, prices[j] - prices[i])
+        //     }
+        // }
+
+        // return maxProfit
+
+        /**
+         * Two pointers
+         */
+        let left = 0
+        let right = left + 1
+        let maxProfit = 0
+        while (right < prices.length) {
+            maxProfit = Math.max(maxProfit, prices[right] - prices[left])
+            if (prices[left] > prices[right]) left++
+            else right++
+        }
+
+        return maxProfit
+    }
+}
